eb2ad25777a3eb67aa2c0dc2a283ea7b1783d553
Nantaphop Phuengphae <nantaphop.phuengphae@krungthai-axa.co.th>
2019-09-26 15:55:40 +0700

    change

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 require('dotenv').config()
 const app = require('express')()
-app.get('/', (req, res) => res.send("Hello"))
+app.get('/', (req, res) => res.send("Hello OC"))
 app.listen(process.env.PORT, () => console.info(`Server listening at ${process.env.PORT}`))
